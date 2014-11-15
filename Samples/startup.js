d24055e7ed05bbae329512e912f643a70460dcee
StephenWalther <stephen.walther@superexpert.com>
2014-11-14 21:26:32 -0600

    fixed sample styles

--- Samples/startup.js
+++ Samples/startup.js
@@ -10,11 +10,11 @@ if (Meteor.isServer) {
     Meteor.startup(function() {
         if (db.movies.find().count() === 0) {
             var seed = [
-                {title: 'Bladerunner'},
-                {title: 'The Godfather'},
-                {title: 'Inception'},
-                {title: 'The Rocketeer'},
-                {title: 'The Black Hole'},
+                //{title: 'Bladerunner'},
+                //{title: 'The Godfather'},
+                //{title: 'Inception'},
+                //{title: 'The Rocketeer'},
+                //{title: 'The Black Hole'},
                 {title: 'King Kong'},
                 {title: 'Star Wars'},
                 {title: 'King Kong'},
